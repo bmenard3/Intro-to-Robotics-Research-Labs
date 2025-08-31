4de7c9ce3f2c2f0a8ab2ec348b92437f17263eae
August Menard <bmenard3@gatech.edu>
2025-08-31 12:51:15 -0400

    Added Lab 1 workspace

---
+++ Lab1_ws/build/turtlesim/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/augustmenard/Intro-to-Robotics-Research-Labs/Lab1_ws/src/ros_tutorials/turtlesim
+# Build directory: /home/augustmenard/Intro-to-Robotics-Research-Labs/Lab1_ws/build/turtlesim
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("turtlesim__py")
