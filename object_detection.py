889ef024b83b18c8cd226c1a6c1d0838484239e5
August Menard <bmenard3@gatech.edu>
2025-09-03 17:27:55 -0400

    Finished Object Tracking based on Color Threshold

--- object_detection.py
+++ object_detection.py
@@ -1,25 +1,40 @@
 import cv2
 import numpy as np
+from PIL import Image
 
-cap = VideoCapture(0)
+cap = cv2.VideoCapture(0)
+params = cv2.SimpleBlobDetector_Params()
+
 
 while(1):
     _, frame = cap.read()
 
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    lower_blue = np.array([110,50,50])
-    upper_blue = np.array([130,255,255])
+    lower_limit = np.array([100,75,75])
+    upper_limit = np.array([120,255,255])
 
-    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    mask = cv2.inRange(hsv, lower_limit, upper_limit)
 
-    res = cv2.inRange(hsv, lower_blue, upper_blue)
+    masked_frame = cv2.bitwise_and(frame,frame, mask=mask)
+    inv_mask = cv2.bitwise_not(mask)
+
+    mask_img = Image.fromarray(mask)
+    bbox = mask_img.getbbox()
+
+    if bbox is not None:
+        x1, y1, x2, y2 = bbox
+        xc = int((x1 + x2) / 2)
+        yc = int((y1 + y2) / 2)
+        frame = cv2.circle(frame, (xc, yc), 10, (0,0,255), 3)
 
     cv2.imshow('frame', frame)
-    cv2.imshow('res',res)
+    cv2.imshow('mask', mask)
+    #cv2.imshow('res', res)
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
         break
 
+cap.release()
 cv2.destroyAllWindows()
 
