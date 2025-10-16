a5f363cc995430638bb2a758f84f60e42f879405
August Menard <bmenard3@gatech.edu>
2025-10-16 17:48:10 -0400

    gains

--- lab4_ws/install/team_rocket_navigate_to_goal/lib/python3.10/site-packages/team_rocket_navigate_to_goal/goToGoal.py
+++ lab4_ws/install/team_rocket_navigate_to_goal/lib/python3.10/site-packages/team_rocket_navigate_to_goal/goToGoal.py
@@ -94,7 +94,7 @@ class GoToGoal(Node):
 
         self.angular_pid = PIDController(
             kp=2.0,
-            ki=0.0,
+            ki=2.0,
             kd=0.0,
             integral_limit=0.5
         )
