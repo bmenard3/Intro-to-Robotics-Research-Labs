cc67f9153c4d630cd2115312d6f17d089f046387
Leeeo547 <hliu852@gatech.edu>
2025-10-17 12:10:49 -0400

    gain adjust

--- lab4_ws/src/team_rocket_navigate_to_goal/team_rocket_navigate_to_goal/goToGoal.py
+++ lab4_ws/src/team_rocket_navigate_to_goal/team_rocket_navigate_to_goal/goToGoal.py
@@ -172,7 +172,7 @@ class GoToGoal(Node):
                 self.wait_start = time.time()
 
         elif self.state == 1: # Avoid Obstacles
-            if abs(closest_angle - (math.pi / 2)) > 0.2:
+            if abs(closest_angle - (math.pi / 2)) > 0.27:
                 self.get_logger().info(f'State 1: turning, {abs(closest_angle - (math.pi / 2))}')
                 msg.linear.x = 0.0
                 msg.linear.y = 0.0
