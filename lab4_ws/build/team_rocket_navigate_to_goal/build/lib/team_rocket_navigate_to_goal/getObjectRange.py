af9362c8060a7e645b47f97633172a68c58bd143
Leeeo547 <hliu852@gatech.edu>
2025-10-17 10:29:40 -0400

    lidar logging

--- lab4_ws/build/team_rocket_navigate_to_goal/build/lib/team_rocket_navigate_to_goal/getObjectRange.py
+++ lab4_ws/build/team_rocket_navigate_to_goal/build/lib/team_rocket_navigate_to_goal/getObjectRange.py
@@ -33,7 +33,7 @@ class GetObjectRange(Node):
         self.angle_min = msg.angle_min
         self.angle_max = msg.angle_max
         self.ranges = msg.ranges
-        self.get_logger().info(f'angle_min={self.angle_min}, angle_max={self.angle_max}, min range:{min(self.ranges)}')
+        self.get_logger().info(f'angle_min={self.angle_min}, increment={msg.angle_increment}')
     
     def timer_callback(self):
         msg = Float32()
