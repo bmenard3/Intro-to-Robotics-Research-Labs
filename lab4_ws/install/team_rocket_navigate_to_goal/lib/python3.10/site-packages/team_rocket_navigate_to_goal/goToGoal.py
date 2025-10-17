3e3539032be3a05d9c400c30e572c68acbc117e3
August Menard <bmenard3@gatech.edu>
2025-10-17 12:28:02 -0400

    gain fixes

--- lab4_ws/install/team_rocket_navigate_to_goal/lib/python3.10/site-packages/team_rocket_navigate_to_goal/goToGoal.py
+++ lab4_ws/install/team_rocket_navigate_to_goal/lib/python3.10/site-packages/team_rocket_navigate_to_goal/goToGoal.py
@@ -94,7 +94,7 @@ class GoToGoal(Node):
         self.angles = [math.pi]
 
         #self.waypoints = np.array([[1.5, 0], [1.5, 1.4], [0, 1.4]])
-        self.waypoints = np.array([[1.0, 0.0], [1.0, 1.0]])
+        self.waypoints = np.array([[1.5, 0.0], [1.5, 1.4]])
         self.current_goal = 0
         self.goal_pos = Point()
         self.goal_pos.x = 0.0
