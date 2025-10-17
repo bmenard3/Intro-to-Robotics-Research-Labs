ab99e3ec724793c530de0987bb6a243c4a4b04cf
Leeeo547 <hliu852@gatech.edu>
2025-10-17 10:45:13 -0400

    fix

--- lab4_ws/install/team_rocket_navigate_to_goal/lib/python3.10/site-packages/team_rocket_navigate_to_goal/getObjectRange.py
+++ lab4_ws/install/team_rocket_navigate_to_goal/lib/python3.10/site-packages/team_rocket_navigate_to_goal/getObjectRange.py
@@ -35,8 +35,8 @@ class GetObjectRange(Node):
         self.angle_increment = msg.angle_increment
         self.ranges = msg.ranges
         self.angles = list(np.arange(self.angle_min, self.angle_max, self.angle_increment))
-	min_index = self.ranges.index(min(self.ranges))
-        self.get_logger().info(f'min range = {self.ranges[min_index]}, min_angle = {self.angles[min_index]')
+        min_index = self.ranges.index(min(self.ranges))
+        self.get_logger().info(f'min range = {self.ranges[min_index]}, min_angle = {self.angles[min_index]}')
     
     def timer_callback(self):
         msg = Float32()
